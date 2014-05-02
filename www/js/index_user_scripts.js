3716c357e68a3a755a59e978fe9334ef570e7820
Ashutosh <216.ashutosh@gmail.com>
2014-05-02 11:12:31 +0530

    js

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -4,7 +4,7 @@ function register_event_handlers()
       
          $(document).on("click", "#one-screen", function(evt)
         {
-       //     $.ui.showMask('Doing work')
+            $.ui.showMask('Doing work')
             //Get Prev Data
             var admin_email = $('input#admin_email').val(); //Get email and check if that is true
             var admin_pin = $('input#admin_pin').val();
