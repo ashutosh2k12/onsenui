3458649b59effc300aba4f57ef2c161aadb52640
ashutosh2k12 <216.ashutosh@gmail.com>
2014-05-02 19:38:16 +0530

    new js fix

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -7,7 +7,7 @@ function register_event_handlers()
             //Get Prev Data
             var admin_email = $('input#admin_email').val(); //Get email and check if that is true
             var admin_pin = $('input#admin_pin').val();
-
+			alert('token='+token);
 			//Check data by ajax
 			$.ajax({
 			   type: "POST",
