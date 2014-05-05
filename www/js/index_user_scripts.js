e0ddf9b03f790de856a33a7200df2031bdbba71c
ashutosh2k12 <216.ashutosh@gmail.com>
2014-05-05 08:11:05 +0530

    xdk

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,102 @@
+var token; //The Token
+
+function register_event_handlers()
+ {   
+         $(document).on("click", "#one-screen", function(evt)
+        {
+            //Get Prev Data
+            var admin_email = $('input#admin_email').val(); //Get email and check if that is true
+            var admin_pin = $('input#admin_pin').val();
+			alert('token='+token);
+			//Check data
+			$.ajax({
+			   type: "POST",
+			   url: "http://sumitjaiswal.com/area51/notifi/admin/rest/user",
+			   data: {devid: token, name: admin_pin, email:admin_email},
+			   dataType: "json",
+			   success: function(data) {
+					 if(data.error==0){
+						if(data.hardware==true){	//Goto Page1
+						$.ui.loadContent("#uib_page_1",false,false,"slide"); }
+						else{	alert('you\'re new'); }
+					 }
+					 else{
+						alert('Error: '+data.error_response);
+						return false;
+					 }
+					 
+			   },
+			   error: function(xhr, ajaxOptions, thrownError) {
+					 alert(xhr.status);
+					 alert(thrownError);
+			   }
+			})
+        });
+       
+
+}
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+    //    initPushPlug();
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+		register_event_handlers();
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"16692000019","ecb":"app.onNotificationGCM"});
+    },
+        // result contains any message sent from the plugin call
+        successHandler: function(result) {
+                alert('Callback Success! Result = '+result)
+        },
+        //Any errors? 
+        errorHandler:function(error) {
+                alert(error);
+        },
+        onNotificationGCM: function(e) {
+
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+					token = e.regid;
+                }
+            break;
+ 
+            case 'message':
+						  // this is the actual push notification. its format depends on the data model from the push server
+						  alert('push message = '+e.message);
+                          
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    }
+};
+
+ 
+
